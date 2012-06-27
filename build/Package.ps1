9f29ae1208c598dba372494ae33498ed73428814
Brendan Forster <me@brendanforster.com>
2012-06-27 23:03:44 +1000

    updated build

--- build/Package.ps1
+++ build/Package.ps1
@@ -6,7 +6,7 @@ properties {
     $OutputDir = "$BaseDir\Deploy\Package\"
     $ArtifactsDir = "$BaseDir\artifacts\"
     $NuGetPackDir = Join-Path "$OutputDir" "Pack\"
-    $BaseVersion = "0.0.1"
+    $BaseVersion = "0.0.2"
     $Version = "$BaseVersion-pre" 
     $PatchNumber = (git rev-list --all | wc -l).trim() # TODO: better implementation
     $AssemblyVersion = "$BaseVersion.$PatchNumber"
